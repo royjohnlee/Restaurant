5e56c2512700c6e628140bb0cba5363d859b9c12
royjohnlee <royjohnlee@DESKTOP-BO5FP47.localdomain>
2023-02-13 01:32:54 -0800

    restaurant index finished vid5

--- controllers/restaurants.js
+++ controllers/restaurants.js
@@ -1,6 +1,26 @@
+const Restaurant = require('../models/restaurant')
 module.exports = {
-    new: newRestaurant
+    index,
+    new: newRestaurant,
+    create
 };
+function index(req, res) {
+    Restaurant.find({}, function (error, restaurants) {
+        res.render('restaurants/index', { restaurants })
+    });
+
+}
+
+function create(req, res) {
+    const restaurant = new Restaurant(req.body);
+    restaurant.save(function (error) {
+        if (error) {
+            return res.redirect('/restaurants/new')
+        }
+        console.log(restaurant);
+        res.redirect('/restaurants/new')
+    })
+}
 
 function newRestaurant(req, res) {
     res.render('restaurants/new');
--- routes/restaurants.js
+++ routes/restaurants.js
@@ -3,8 +3,11 @@ var router = express.Router();
 var restaurantCtrl = require('../controllers/restaurants');
 
 // All routes starts with '/restaurant'
+router.get('/', restaurantCtrl.index)
 
 // GET /restaurants/new
 router.get('/new', restaurantCtrl.new)
+// POST /restaurant
+router.get('/', restaurantCtrl.create)
 
 module.exports = router;
